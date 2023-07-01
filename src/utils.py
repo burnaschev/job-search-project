6681575b9f12bc1d6390619538dae68d12174a2a
Артур <informal201144@gmai;.com>
2023-07-01 19:05:12 +0500

    Fixed bugs

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,8 @@
-def filter_vacancies(hh_vacancies, superjob_vacancies, filter_words) -> list:
+def filter_vacancies(hh_vacancies: list, superjob_vacancies: list, filter_words: str) -> list:
     """ Создание списка вакансий
     """
     hh_list = []
-    for vacancies_hh in hh_vacancies["items"]:
+    for vacancies_hh in hh_vacancies:
         if filter_words.lower() in vacancies_hh["name"].lower():
             if vacancies_hh['salary'] is not None:
                 if vacancies_hh['salary']["from"] is None:
@@ -24,10 +24,10 @@ def filter_vacancies(hh_vacancies, superjob_vacancies, filter_words) -> list:
                 hh_list.append(
                     f'{vacancies_hh["name"]}\n'
                     f'Заработная плата: по результатам собеседования.\n'
-                    f'Требуемый опыт: {vacancies_hh["experience"]["name"]}'
+                    f'Требуемый опыт: {vacancies_hh["experience"]["name"]}\n'
                     f'Ссылка на вакансию: {vacancies_hh["alternate_url"]}\n')
     sj_list = []
-    for vacancies_sj in superjob_vacancies['objects']:
+    for vacancies_sj in superjob_vacancies:
         if filter_words.lower() in vacancies_sj['profession'].lower():
             if vacancies_sj['payment_from'] == 0:
                 sj_list.append(f"{vacancies_sj['profession']}\n"
@@ -47,20 +47,20 @@ def filter_vacancies(hh_vacancies, superjob_vacancies, filter_words) -> list:
     vacancies = []
     vacancies.extend(sj_list)
     vacancies.extend(hh_list)
-    return sj_list
+    return vacancies
 
 
-def sort_vacancies(vacancies):
+def sort_vacancies(vacancies: list):
     """ Сортировка вакансий"""
     return sorted(vacancies)
 
 
-def get_top_vacancies(vacancies, top_n):
+def get_top_vacancies(vacancies: list, top_n: int):
     """Первые указанные топ вакансий"""
     return vacancies[:top_n + 1]
 
 
-def print_vacancies(vacancies):
+def print_vacancies(vacancies: list):
     """Вывод вакансий"""
     for vacancy in vacancies:
         print(vacancy)
